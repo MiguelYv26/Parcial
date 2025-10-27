e76cac00facc7afd390d6737f7b1f078732a6422
MiguelYv26 <miyava1995@gmail.com>
2025-10-26 19:01:47 -0500

    feat(entities): add Task and Student base classes

---
+++ src/main/java/com/example/SpringbootBasic/entities/Student.java
@@ -0,0 +1,41 @@
+package com.example.SpringbootBasic.entities;
+
+public class Student {
+    private Long id;
+    private String nombre;
+    private String email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
---
+++ src/main/java/com/example/SpringbootBasic/entities/Task.java
@@ -0,0 +1,74 @@
+package com.example.SpringbootBasic.entities;
+
+import java.time.LocalDate;
+
+public class Task {
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private LocalDate fechaEntrega;
+    private boolean entregada;
+    private Long studentId;
+
+    public Task() {
+    }
+
+    public Task(Long id, String titulo, String descripcion, LocalDate fechaEntrega, boolean entregada, Long studentId) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.fechaEntrega = fechaEntrega;
+        this.entregada = entregada;
+        this.studentId = studentId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(LocalDate fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public boolean isEntregada() {
+        return entregada;
+    }
+
+    public void setEntregada(boolean entregada) {
+        this.entregada = entregada;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+}
+
+
